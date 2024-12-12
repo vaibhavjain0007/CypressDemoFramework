49d37d9b5406b50754f29e4788539ac54359662b
Vaibhav Jain <vaibhav25011998@gmail.com>
2024-12-12 16:22:16 +0530

    first commit

---
+++ cypress/support/pages/HomePage.js
@@ -0,0 +1,29 @@
+class HomePage {
+    // Locators
+    get signIn() {
+        return cy.get('#nav-link-accountList')
+    }
+
+    get orders() {
+        return cy.get('#nav-orders')
+    }
+
+    get search() {
+        return cy.get('#twotabsearchtextbox')
+    }
+
+    // Actions
+    clickSignIn() {
+        this.signIn.click()
+    }
+
+    searchProduct(productName) {
+        this.search.type(productName )
+    }
+
+    navigateToOrders() {
+        this.orders().click()
+    }
+}
+
+export default HomePage;
